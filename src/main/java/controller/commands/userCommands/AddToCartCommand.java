fe07302268613963189e8ebdb6e2f3c74607daae
Slava Provalov <provalov1@gmail.com>
2017-10-09 04:32:34 +0300

    now session stores items, that user already have

--- src/main/java/controller/commands/userCommands/AddToCartCommand.java
+++ src/main/java/controller/commands/userCommands/AddToCartCommand.java
@@ -1,28 +1,33 @@
 package controller.commands.userCommands;
 
-import controller.SessionContent;
+import controller.ContextStorage;
 import controller.commands.ActionCommand;
 import controller.resourceManager.ConfigurationManager;
+import model.entity.Periodical;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public class AddToCartCommand implements ActionCommand {
-    private static SessionContent sessionContent;
+    private static ContextStorage contextStorage;
 
     public AddToCartCommand() {
-        sessionContent = SessionContent.getInstance();
+        contextStorage = ContextStorage.getInstance();
     }
 
     @Override
     public String execute(HttpServletRequest request) {
         HttpSession session = request.getSession();
         int periodicalId = Integer.parseInt(request.getParameter("pItem_id"));
-        if (!sessionContent.getUserPeriodicals().contains(sessionContent.getPeriodicals().get(periodicalId))) {
-            sessionContent.getBuyList().add(sessionContent.getPeriodicals().get(periodicalId));
-            session.setAttribute("items", sessionContent.getBuyList().size());
-            session.setAttribute("cart", sessionContent.getBuyList());
+        List<Periodical> userPeriodicals = (List<Periodical>) session.getAttribute("userPeriodicals");
+        if (!userPeriodicals.contains(contextStorage.getPeriodicals().get(periodicalId))) {
+            Set<Periodical> cart = ((Set<Periodical>) session.getAttribute("cart"));
+            cart.add(contextStorage.getPeriodicals().get(periodicalId));
+            session.setAttribute("items", cart.size());
+            session.setAttribute("cart", cart);
         }
         Optional<String> page = Optional.of(ConfigurationManager.getProperty("path.servlet.main"));
         return page.get();
