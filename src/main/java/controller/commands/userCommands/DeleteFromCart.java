15323a3441a38f66be7b3cef6c592573c49983fe
Slava Provalov <provalov1@gmail.com>
2017-10-09 04:56:36 +0300

    refactoring

--- src/main/java/controller/commands/userCommands/DeleteFromCart.java
+++ src/main/java/controller/commands/userCommands/DeleteFromCart.java
@@ -1,25 +1,32 @@
 package controller.commands.userCommands;
 
-import controller.SessionContent;
+import controller.ContextStorage;
 import controller.commands.ActionCommand;
 import controller.resourceManager.ConfigurationManager;
+import controller.resourceManager.PageContextManager;
+import model.entity.Periodical;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
+import java.util.Set;
 
 public class DeleteFromCart implements ActionCommand {
-    private static SessionContent sessionContent;
+    private static ContextStorage contextStorage;
 
     public DeleteFromCart() {
-        sessionContent = SessionContent.getInstance();
+        contextStorage = ContextStorage.getInstance();
     }
 
     @Override
     public String execute(HttpServletRequest request) {
         int id = Integer.parseInt(request.getParameter("Item_id"));
-        sessionContent.getBuyList().remove(sessionContent.getPeriodicals().get(id));
-        request.getSession().setAttribute("cart", sessionContent.getBuyList());
-        request.getSession().setAttribute("items", sessionContent.getBuyList().size());
+        HttpSession session = request.getSession();
+        Set<Periodical> cart = (Set<Periodical>) session.getAttribute("cart");
+        cart.remove(contextStorage.getPeriodicals().get(id));
+        request.getSession().setAttribute("cart", cart);
+        request.getSession().setAttribute("items", cart.size() > 0 ?
+                cart.size() : ((PageContextManager) session.getAttribute("pageContextManager")).getProperty("cart.empty"));
         Optional<String> page = Optional.of(ConfigurationManager.getProperty("path.servlet.cart"));
         return page.get();
     }
