d5d68066d5eab8d5cc83c6a8c8633c1596fdf2e8
Slava Provalov <provalov1@gmail.com>
2017-10-10 14:00:39 +0300

    used optional for avoiding nulls

--- src/main/java/model/service/MD5.java
+++ src/main/java/model/service/MD5.java
@@ -2,11 +2,14 @@ package model.service;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.util.Optional;
+
 /**
  * Created by Provalov on 23.09.2017.
  */
 public class MD5 {
     public static String md5Hex(String pass) {
-        return DigestUtils.md5Hex(pass);
+        Optional<String> result = Optional.ofNullable(pass);
+        return DigestUtils.md5Hex(result.get());
     }
 }
