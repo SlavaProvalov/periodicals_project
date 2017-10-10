bf2e36bfc188d919f9892975ce690a89f5616bed
Slava Provalov <provalov1@gmail.com>
2017-10-10 13:53:23 +0300

    refactoring

--- src/main/java/controller/commands/ActionCommand.java
+++ src/main/java/controller/commands/ActionCommand.java
@@ -3,5 +3,5 @@ package controller.commands;
 import javax.servlet.http.HttpServletRequest;
 
 public interface ActionCommand {
-    public String execute(HttpServletRequest request);
+    String execute(HttpServletRequest request);
 }
