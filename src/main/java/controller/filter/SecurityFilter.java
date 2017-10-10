7a6bea08e8974240f4b07d874e69e51fd99b7023
Slava Provalov <provalov1@gmail.com>
2017-10-10 14:01:34 +0300

    added logger, error page return correctly now

--- src/main/java/controller/filter/SecurityFilter.java
+++ src/main/java/controller/filter/SecurityFilter.java
@@ -3,7 +3,8 @@ package controller.filter;
 import controller.resourceManager.ConfigurationManager;
 import controller.resourceManager.MessageManager;
 import controller.resourceManager.PageContextManager;
-import controller.utils.UriConverter;
+import controller.utils.StringConverter;
+import org.apache.log4j.Logger;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -20,19 +21,22 @@ import static model.entity.Role.*;
         dispatcherTypes = {DispatcherType.FORWARD, DispatcherType.REQUEST},
         servletNames = {"Controller"})
 public class SecurityFilter implements Filter {
+    private static final Logger log = Logger.getLogger(SecurityFilter.class);
     private static List<String> adminAllowedCommands;
     private static List<String> userAllowedCommands;
     private static List<String> guestAllowedCommands;
     private static String errorPage;
 
     public SecurityFilter() {
-        adminAllowedCommands = Arrays.asList("logout", "user_details", "user_update", "user_update_page", "new_periodical",
+        adminAllowedCommands = Arrays.asList("logout", "user_details", "user_update",
+                "user_update_page", "new_periodical", "check_all_orders",
                 "new_periodical_page", "main", "add_to_cart",
                 "delete_from_cart", "cart", "order_page",
                 "order", "order_confirm", "success_page",
                 "change_language", "error", "");
 
-        userAllowedCommands = Arrays.asList("logout", "user_details", "user_update", "user_update_page", "main",
+        userAllowedCommands = Arrays.asList("logout", "user_details", "user_update",
+                "user_update_page", "main",
                 "add_to_cart", "delete_from_cart", "cart",
                 "order_page", "order", "order_confirm",
                 "success_page", "change_language", "error", "user_delete");
@@ -41,7 +45,6 @@ public class SecurityFilter implements Filter {
                 "login", "signup_page", "signup",
                 "success_page", "change_language", "", "error");
         errorPage = "path.servlet.error";
-
     }
 
 
@@ -51,48 +54,49 @@ public class SecurityFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         HttpSession session = httpRequest.getSession();
         String role = ((String) session.getAttribute("role"));
         if (ADMIN.name().equals(role)) {
-            allowedCommands(chain, httpRequest, httpResponse, adminAllowedCommands, errorPage);
+            allowedCommands(chain, httpRequest, httpResponse, adminAllowedCommands);
         } else if (USER.name().equals(role)) {
-            allowedCommands(chain, httpRequest, httpResponse, userAllowedCommands, errorPage);
+            allowedCommands(chain, httpRequest, httpResponse, userAllowedCommands);
         } else {
             if (!GUEST.name().equals(role)) {
-                initDefault(session, role);
+                initDefault(session);
             }
-            allowedCommands(chain, httpRequest, httpResponse, guestAllowedCommands, errorPage);
+            allowedCommands(chain, httpRequest, httpResponse, guestAllowedCommands);
         }
     }
 
-    private void initDefault(HttpSession session, String role) {
-        role = GUEST.name();
-        session.setAttribute("role", role);
+    private void initDefault(HttpSession session) {
+        session.setAttribute("role", GUEST.name());
         session.setAttribute("language", "en");
         session.setAttribute("pageContextManager", new PageContextManager("en"));
         session.setAttribute("messageManager", new MessageManager("en"));
     }
 
-    private void allowedCommands(FilterChain chain, HttpServletRequest httpRequest, HttpServletResponse httpResponse, List<String> allowedCommands, String redirectPage) throws IOException, ServletException {
-        List<String> ac = allowedCommands;
-//--------------DEBUG---------------------
-//        String action = UriConverter.uriToForFilter(httpRequest);
-//        boolean contains = ac.contains(action);
-//        System.out.println(action);
-//        System.out.println(contains);
-//----------------------------------------
-        if (ac.contains(UriConverter.uriToForFilter(httpRequest))) {
-            chain.doFilter(httpRequest, httpResponse);
-        } else {
-            httpRequest.getSession().setAttribute("currentPage", httpRequest.getRequestURI());
-            RequestDispatcher dispatcher = httpRequest.getServletContext().getRequestDispatcher(ConfigurationManager.getProperty(redirectPage));
-            dispatcher.forward(httpRequest, httpResponse);
-        }
+    private String getRedirectPage(HttpServletRequest request) {
+        return request.getSession().getAttribute("role").equals(GUEST.name()) ? "path.servlet.welcome" : "path.servlet.main";
     }
 
+    private void allowedCommands(FilterChain chain, HttpServletRequest request, HttpServletResponse response,
+                                 List<String> allowedCommands) throws IOException, ServletException {
+        List<String> ac = allowedCommands;
+        if (ac.contains(StringConverter.uriToForFilter(request))) {
+            chain.doFilter(request, response);
+        } else {
+            log.warn("attempt of restricted access to: " + request.getRequestURI());
+            request.setAttribute("redirectPage", getRedirectPage(request));
+            request.setAttribute("restrictedPage", request.getRequestURI());
+            RequestDispatcher dispatcher = request.getServletContext()
+                    .getRequestDispatcher(ConfigurationManager.getProperty(errorPage));
+            dispatcher.forward(request, response);
+        }
+    }
 
     @Override
     public void destroy() {
