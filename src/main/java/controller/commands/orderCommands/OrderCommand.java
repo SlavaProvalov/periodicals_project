0f438c232a8c5359fb4fd8d5eef137d712f7e086
Slava Provalov <provalov1@gmail.com>
2017-10-10 14:04:50 +0300

    responsibility of building order from request parameters now gone to EntityBuilder class

--- src/main/java/controller/commands/orderCommands/OrderCommand.java
+++ src/main/java/controller/commands/orderCommands/OrderCommand.java
@@ -2,19 +2,19 @@ package controller.commands.orderCommands;
 
 import controller.commands.ActionCommand;
 import controller.resourceManager.ConfigurationManager;
+import controller.utils.EntityBuilder;
 import controller.utils.ErrorConstructor;
-import model.entity.Periodical;
-import model.entity.builder.OrderBuilder;
+import model.entity.Order;
 import model.service.OrderService;
+import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
 import java.util.Optional;
-import java.util.Set;
 
 public class OrderCommand implements ActionCommand {
+    private static final Logger log = Logger.getLogger(OrderCommand.class);
     private static OrderService service;
 
     public OrderCommand() {
@@ -23,32 +23,16 @@ public class OrderCommand implements ActionCommand {
 
     @Override
     public String execute(HttpServletRequest request) {
-        Optional<String> page = Optional.empty();
+        Optional<String> page;
         HttpSession session = request.getSession();
         try {
-            int id = (int) session.getAttribute("userId");
-            String address = request.getParameter("address");
-            String postalCode = request.getParameter("postalCode");
-            String city = request.getParameter("city");
-            String country = request.getParameter("country");
-            session.setAttribute("address_value", address);
-            session.setAttribute("postalCode_value", postalCode);
-            session.setAttribute("city_value", city);
-            session.setAttribute("country_value", country);
-            OrderBuilder builder = new OrderBuilder();
-            service.createNewOrder(builder.createNewOrder()
-                    .setClientId(id)
-                    .setOrderDate(LocalDateTime.now())
-                    .setAddress(address)
-                    .setCity(city)
-                    .setPostalCode(postalCode)
-                    .setCountry(country)
-                    .setPeriodicals((Set<Periodical>) session.getAttribute("cart")).getOrder());
+            Order order = EntityBuilder.createOrderFromRequest(request, session);
+            service.createNewOrder(order);
             page = Optional.of(ConfigurationManager.getProperty("path.servlet.order_confirm"));
         } catch (SQLException e) {
-            e.printStackTrace(); // TODO: 02.10.2017 log
+            log.error(e);
             page = Optional.of(ConfigurationManager.getProperty("path.page.error"));
-            ErrorConstructor.fillErrorMessage(request, e, "path.servlet.cart");
+            ErrorConstructor.fillErrorPage(request, e, "path.servlet.cart");
         }
         return page.get();
     }
